3641281ff52614b911bad66fbef9ba13cb409a55
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-23 10:21:51 -0300

    Referencia list view

--- app/src/main/java/br/com/fiap/a2tina_android_listview/MainActivity.java
+++ app/src/main/java/br/com/fiap/a2tina_android_listview/MainActivity.java
@@ -2,12 +2,17 @@ package br.com.fiap.a2tina_android_listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lstContatos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lstContatos = (ListView)findViewById(R.id.lstContatos);
     }
 }
